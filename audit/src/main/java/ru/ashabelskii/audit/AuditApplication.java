b39da07e5fc911ba0cc1c9e66973b2c564c8f54d
ashabelskii <ashabelskii@neoflex.ru>
2023-09-06 10:17:56 +0300

    audit init

---
+++ audit/src/main/java/ru/ashabelskii/audit/AuditApplication.java
@@ -0,0 +1,13 @@
+package ru.ashabelskii.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditApplication.class, args);
+    }
+
+}
